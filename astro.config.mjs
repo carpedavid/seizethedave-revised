b27092b09ac6db1c16aca84e6968745f5378921f
David Garrett <dave@carpedavid.com>
2025-09-13 23:53:06 -0400

    added sitemap and rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  site: 'https://carpedavid.com',
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()]
 });
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,28 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+    const blog = await getCollection('blog');
+  return rss({
+    title: 'Seize the Dave',
+    description: 'A little bit about a lot of stuff',
+    site: context.site,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `id`
+      // This example assumes all posts are rendered as `/blog/[id]` routes
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
